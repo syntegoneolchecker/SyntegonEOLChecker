0c33850c697758a20098296c394862fbb05e9e72
syntegoneolchecker <syntegoneolchecker@gmail.com>
2025-11-27 13:46:05 +0900

    Update get-groq-usage.js

--- netlify/functions/get-groq-usage.js
+++ netlify/functions/get-groq-usage.js
@@ -11,7 +11,7 @@ exports.handler = async function(event, context) {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    model: 'llama-3.3-70b-versatile',
+                    model: 'meta-llama/llama-4-maverick-17b-128e-instruct',
                     messages: [
                         {
                             role: 'user',
