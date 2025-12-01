cdd0b0a9eae48efa8e5937bc64a88cd24e00bb74
syntegoneolchecker <syntegoneolchecker@gmail.com>
2025-12-01 16:10:11 +0900

    Update initialize-job.js

--- netlify/functions/initialize-job.js
+++ netlify/functions/initialize-job.js
@@ -56,7 +56,7 @@ exports.handler = async function(event, context) {
                 max_results: 2,  // 2 URLs to stay within token limits
                 // NOTE: Removed include_raw_content - we'll scrape with Render instead
                 include_domains: [
-                    'kvm-switches-online.com'
+                    'kvm-switches-online.com',
                     'daitron.co.jp',
                     'kdwan.co.jp',
                     'hewtech.co.jp',
