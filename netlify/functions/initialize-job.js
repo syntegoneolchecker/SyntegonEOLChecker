99e32ea532d97920691a7a8a1ba57d9f2addc3e5
syntegoneolchecker <syntegoneolchecker@gmail.com>
2025-12-04 14:48:14 +0900

    Update initialize-job.js

--- netlify/functions/initialize-job.js
+++ netlify/functions/initialize-job.js
@@ -309,6 +309,7 @@ exports.handler = async function(event, context) {
                 max_results: 2,  // 2 URLs to stay within token limits
                 // NOTE: Removed include_raw_content - we'll scrape with Render instead
                 include_domains: [
+                    'takigen.co.jp',
                     'mitsubishielectric.co.jp',
                     'sentei.nissei-gtr.co.jp',
                     'orimvexta.co.jp',
